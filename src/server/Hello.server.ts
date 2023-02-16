f24e6008d132787b720d7cb1673d51cc28bbb865
chelizichen <1347290221@qq.com>
2023-02-16 18:21:56 +0800

    测试对象写法

--- src/server/Hello.server.ts
+++ src/server/Hello.server.ts
@@ -3,25 +3,30 @@ import { RpcServerController,Register } from "ado-node";
 /**
  * @description 用于编写 Hello 接口的配置文件
  */
-@RpcServerController("Hello")
+@RpcServerController("HelloInterFace")
 class Hello {
-    
-
     @Register("hello")
     async hello(name:string,age:string,value:string):Promise<{message:string}>{
-        let message = name + age + value
+        console.log(value);
+        let message = name + age
         return { message };
     }
-
-    
-
     @Register("Blow")
     async Blow(name:string,age:string,value:string):Promise<{message:string}>{
         let message = name + age + value
         return { message };
     }
 
-    
+    @Register("TestRet")
+    async TestRet(name: string, age: string, [song, time]: any) {
+        console.log(name);
+        console.log(age);
+        console.log(song);
+        console.log(time);
+        return {
+            message:"Hello World"
+        }
+    }
 }
 
 
